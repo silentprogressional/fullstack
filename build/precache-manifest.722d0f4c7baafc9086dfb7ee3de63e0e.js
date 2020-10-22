8e4e5fda3ccaac232876fd5c05d4e5c1159ac9f0
unknown <astrupnate@gmail.com>
2020-10-22 18:29:35 -0400

    added build folder

---
+++ build/precache-manifest.722d0f4c7baafc9086dfb7ee3de63e0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b567bdbd7244d7adade77d31c67662de",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dfaa7fa356217fbe2ec",
+    "url": "/static/css/main.194ca330.chunk.css"
+  },
+  {
+    "revision": "bb7a758186e2fbbe95d5",
+    "url": "/static/js/2.1597beb0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1597beb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dfaa7fa356217fbe2ec",
+    "url": "/static/js/main.15982fde.chunk.js"
+  },
+  {
+    "revision": "0b5e287a74745946f914",
+    "url": "/static/js/runtime-main.1b01eba1.js"
+  }
+]);
